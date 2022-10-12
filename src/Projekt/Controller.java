f8dbd2604151b2a0296848bb3bade7c606dcffec
marcin-lipinski <m.in.lipinski@gmail.com>
2022-10-12 18:21:05 +0200

    Renaming and refactoring, part I

--- src/Projekt/Controller.java
+++ src/Projekt/Controller.java
@@ -6,7 +6,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -23,13 +22,15 @@ public class Controller implements Initializable
     public int speedOfYellowTrain, speedOfRedTrain, speedOfBlueTrain;
     public int timeOfRedDropping, timeOfBlueDropping, timeOfYellowDropping;
     public int numberOfCarts;
-    Thread P1, P2, P3;
-    private int isRunning =1;
-    private int[][] route={{0,1,2,3,4,5,7,9,11,13,15,17,19,25,31,33,35,37,39,41,43,49,50,51,52,53,0},
+
+    private String[][] carsImagesURLs;
+    private Thread P1, P2, P3;
+    private int isRunning = 1;
+    private final int[][] route={{0,1,2,3,4,5,7,9,11,13,15,17,19,25,31,33,35,37,39,41,43,49,50,51,52,53,0},
                           {0,6,8,10,12,14,16,18,20,21,22,23,24,25,31,33,35,37,39,41,43,49,48,47,46,45,0},
                           {0,1,2,3,4,5,7,9,11,13,15,17,19,25,26,27,28,29,30,32,34,36,38,40,42,44,0}};
 
-    private int[][] railwaysXY={{0,0},{530,3}, {590,3},{650,3},{710,3},{770,3},{470,42}, {770,42},
+    private final int[][] railwaysXY={{0,0},{530,3}, {590,3},{650,3},{710,3},{770,3},{470,42}, {770,42},
                                 {470,81}, {770,81},{470,120}, {770,120},{470,159}, {770,159},{470,198}, {770,198},
                                 {470,237}, {770,237}, {470,276}, {770,276}, {470,315}, {530,315},{590,315},{650,315},
                                 {710,315},{770,315}, {830,315},{890,315},{950,315},{1010,315},{1070,315},
@@ -37,11 +38,12 @@ public class Controller implements Initializable
                                 {770,510},{1070,510},{770,549},{1070,549}, {770,588},{1070,588},{530,627},{590,627},
                                 {650,627},{710,627},{770,627}, {830,627},{890,627},{950,627},{1010,627},{1070,627}};
 
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        carsImagesURLs = new String[][]{{"src/resources/yellow_loco.png", "src/resources/yellow_car.png"},
+                                        {"src/resources/red_loco.png", "src/resources/red_car.png"},
+                                        {"src/resources/blue_loco.png", "src/resources/blue_car.png"}};
         startButton.setDisable(true);
         SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(2,12);
         valueFactory.setValue(2);
@@ -49,28 +51,44 @@ public class Controller implements Initializable
         numberOfCarts=numberOfCartsSpinner.getValue();
         numberOfCartsSpinner.valueProperty().addListener((observableValue, integer, t1) -> numberOfCarts=numberOfCartsSpinner.getValue());
 
-        speedOfYellowTrain=(int)(speedOfYellowTrainSlider.getValue());
+        addingListeners();
+        setInitialSpeedOfTrains();
+        setInitialTimeOfDropping();
+        setCarsImages();
+    }
+
+    private void setInitialSpeedOfTrains(){
+        speedOfYellowTrain = (int)(speedOfYellowTrainSlider.getValue());
+        speedOfRedTrain = (int)(speedOfRedTrainSlider.getValue());
+        speedOfBlueTrain = (int)(speedOfBlueTrainSlider.getValue());
+        setInitialSpeedOfTrainsLabels();
+    }
+
+    private void setInitialSpeedOfTrainsLabels(){
         speedOfYellowTrainLabel.setText(Integer.toString(speedOfYellowTrain));
-
-        speedOfRedTrain=(int)(speedOfRedTrainSlider.getValue());
         speedOfRedTrainLabel.setText(Integer.toString(speedOfRedTrain));
-
-        speedOfBlueTrain=(int)(speedOfBlueTrainSlider.getValue());
         speedOfBlueTrainLabel.setText(Integer.toString(speedOfBlueTrain));
+    }
 
-        timeOfRedDropping=(int)(timeOfRedDroppingSlider.getValue());
+    private void setInitialTimeOfDropping(){
+        timeOfRedDropping = (int)(timeOfRedDroppingSlider.getValue());
+        timeOfBlueDropping = (int)(timeOfBlueDroppingSlider.getValue());
+        timeOfYellowDropping = (int)(timeOfYellowDroppingSlider.getValue());
+        setInitialTimeOfDroppingLabels();
+    }
+
+    private void setInitialTimeOfDroppingLabels(){
         timeOfRedDroppingLabel.setText(Integer.toString(timeOfRedDropping));
-
-        timeOfBlueDropping=(int)(timeOfBlueDroppingSlider.getValue());
         timeOfBlueDroppingLabel.setText(Integer.toString(timeOfBlueDropping));
-
-        timeOfYellowDropping=(int)(timeOfYellowDroppingSlider.getValue());
         timeOfYellowDroppingLabel.setText(Integer.toString(timeOfYellowDropping));
-
+    }
+    private void addingListeners(){
         speedOfBlueTrainSlider.valueProperty().addListener((observableValue, number, t1) -> {
             speedOfBlueTrain = (int)speedOfBlueTrainSlider.getValue();speedOfBlueTrainLabel.setText(Integer.toString((int) speedOfBlueTrainSlider.getValue()));});
+
         speedOfRedTrainSlider.valueProperty().addListener((observableValue, number, t1) ->{
             speedOfRedTrain = (int)speedOfRedTrainSlider.getValue();speedOfRedTrainLabel.setText(Integer.toString((int)speedOfRedTrainSlider.getValue()));});
+
         speedOfYellowTrainSlider.valueProperty().addListener((observableValue, number, t1) ->{
             speedOfYellowTrain = (int)speedOfYellowTrainSlider.getValue();speedOfYellowTrainLabel.setText(Integer.toString((int)speedOfYellowTrainSlider.getValue()));});
 
@@ -82,77 +100,78 @@ public class Controller implements Initializable
 
         timeOfBlueDroppingSlider.valueProperty().addListener((observableValue,number, t1) ->{
             timeOfBlueDropping = (int) timeOfBlueDroppingSlider.getValue();timeOfBlueDroppingLabel.setText(Integer.toString((int)timeOfBlueDroppingSlider.getValue()));});
+
     }
 
     @FXML
     public void placeTrains()
     {
+        changeButtonsState();
+        for (int i = 0; i <numberOfCarts; i++)
+        {
+            setSingleCarCoor(0, i, route[0][numberOfCarts - i]);
+            checkIndexAndRotate(0, i, route[0][numberOfCarts - i]);
+
+            setSingleCarCoor(1, i, route[1][26 - numberOfCarts + i]);
+            checkIndexAndRotate(1, i, route[1][26 - numberOfCarts + i]);
+
+            setSingleCarCoor(2, i, route[2][26 - numberOfCarts + i]);
+            checkIndexAndRotate(2, i, route[2][26 - numberOfCarts + i]);
+
+            int finalI = i;
+            Platform.runLater(() -> Main.root.getChildren().add(carts[0][finalI]));
+            Platform.runLater(() -> Main.root.getChildren().add(carts[1][finalI]));
+            Platform.runLater(() -> Main.root.getChildren().add(carts[2][finalI]));
+        }
+    }
+
+    private void setSingleCarCoor(int loco, int car, int coorsIndex){
+        carts[loco][car].setX(railwaysXY[coorsIndex][0]);
+        carts[loco][car].setY(railwaysXY[coorsIndex][1]);
+    }
+
+    private void checkIndexAndRotate(int loco, int car, int coorsIndex){
+        switch (loco){
+            case 0:
+                if((coorsIndex>5 && coorsIndex<20) || (coorsIndex>30 && coorsIndex<45)) carts[0][car].setRotate(90);
+                break;
+            case 1:
+                if((coorsIndex>5 && coorsIndex<20) || (coorsIndex>30 && coorsIndex<45)) carts[1][car].setRotate(-90);
+                break;
+            case 2:
+                if((coorsIndex>5 && coorsIndex<20) || (coorsIndex>30 && coorsIndex<45)) carts[2][car].setRotate(90);
+                if(car == 0) carts[2][car].setRotate(-90);
+                if(coorsIndex>25 && coorsIndex <31) carts[2][car].setRotate(180);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void changeButtonsState(){
+        placeTrainsButton.setDisable(true);
+        startButton.setDisable(false);
+        numberOfCartsSpinner.setDisable(true);
+    }
+    private void setCarsImages(){
         carts = new ImageView[3][numberOfCarts + 1];
-        String sourceOfLocoImage = "zolty_lokom.png";
-        String sourceOfCartImage = "zolty_wagon.png";
         for(int j=0;j<3;j++)
         {
             for (int i = 1; i < numberOfCarts; i++)
             {
-                carts[j][i] = new ImageView();
-                carts[j][i].setImage(new Image(sourceOfCartImage));
-                carts[j][i].setFitHeight(40);
-                carts[j][i].setFitWidth(60);
+                setSingleCar(j, i, carsImagesURLs[j][1]);
             }
-            carts[j][0] = new ImageView();
-            carts[j][0].setImage(new Image(sourceOfLocoImage));
-            carts[j][0].setFitHeight(40);
-            carts[j][0].setFitWidth(60);
-            carts[j][numberOfCarts-1] = new ImageView();
-            carts[j][numberOfCarts-1].setImage(new Image(sourceOfLocoImage));
-            carts[j][numberOfCarts-1].setFitHeight(40);
-            carts[j][numberOfCarts-1].setFitWidth(60);
-            if(numberOfCarts!=1)carts[j][numberOfCarts-1].setRotate(180);
-            sourceOfCartImage = "czerw_wagon.png";
-            sourceOfLocoImage = "czerw_lokom.png";
-            if(j==1)
-            {
-                sourceOfCartImage = "nieb_wagon.png";
-                sourceOfLocoImage = "nieb_lokom.png";
-            }
+            setSingleCar(j, 0, carsImagesURLs[j][0]);
+            setSingleCar(j, numberOfCarts - 1, carsImagesURLs[j][0]);
+            if(numberOfCarts != 1) carts[j][numberOfCarts-1].setRotate(180);
         }
-        placeTrainsButton.setDisable(true);
-        startButton.setDisable(false);
-        numberOfCartsSpinner.setDisable(true);
-        int temp;
-        for (int i = 0; i <numberOfCarts; i++)
-        {
-            temp=route[0][numberOfCarts-i];
-            carts[0][i].setX(railwaysXY[temp][0]);
-            carts[0][i].setY(railwaysXY[temp][1]);
+    }
 
-            if((temp>5 && temp<20) || (temp>30 && temp<45)) carts[0][i].setRotate(90);
-
-            int finalI = i;
-            Platform.runLater(() -> Main.root.getChildren().add(carts[0][finalI]));
-        }
-
-        for (int i = 0; i < numberOfCarts; i++)
-        {
-            temp=route[1][26-numberOfCarts+i];
-            carts[1][i].setX(railwaysXY[temp][0]);
-            carts[1][i].setY(railwaysXY[temp][1]);
-
-            if((temp>5 && temp<20) || (temp>30 && temp<45)) carts[1][i].setRotate(-90);
-
-            int finalI = i;
-            Platform.runLater(() -> Main.root.getChildren().add(carts[1][finalI]));
-
-            temp=route[2][26-numberOfCarts+i];
-            carts[2][i].setX(railwaysXY[temp][0]);
-            carts[2][i].setY(railwaysXY[temp][1]);
-
-            if((temp>5 && temp<20) || (temp>30 && temp<45)) carts[2][i].setRotate(90);
-            if(i==0) carts[2][i].setRotate(-90);
-            if(temp>25 && temp <31) carts[2][i].setRotate(180);
-
-            Platform.runLater(() -> Main.root.getChildren().add(carts[2][finalI]));
-        }
+    private void setSingleCar(int loco, int car, String image){
+        carts[loco][car] = new ImageView();
+        carts[loco][car].setImage(new Image(image));
+        carts[loco][car].setFitHeight(40);
+        carts[loco][car].setFitWidth(60);
     }
 
     @FXML
@@ -170,9 +189,7 @@ public class Controller implements Initializable
         {
             startButton.setText("STOP");
             Shared shared = new Shared(this);
-            P1 = new YellowTrain(shared, numberOfCarts,this, route[0]);
-            P2 = new RedTrain(shared, numberOfCarts,this, route[1]);
-            P3 = new BlueTrain(shared, numberOfCarts,this, route[2]);
+            initThreads(shared);
             P1.start();
             P2.start();
             P3.start();
@@ -183,17 +200,31 @@ public class Controller implements Initializable
             startButton.setDisable(true);
             placeTrainsButton.setDisable(false);
             numberOfCartsSpinner.setDisable(false);
-            P1.stop();
-            P2.stop();
-            P3.stop();
-            for (int i = 0; i < numberOfCarts; i++)
-            {
-                int finalI = i;
-                Platform.runLater(() -> Main.root.getChildren().remove(carts[0][finalI]));
-                Platform.runLater(() -> Main.root.getChildren().remove(carts[1][finalI]));
-                Platform.runLater(() -> Main.root.getChildren().remove(carts[2][finalI]));
-            }
+            stopThreads();
+            removeTrainsFromBoard();
+
         }
+    }
+
+    private void removeTrainsFromBoard(){
+        for (int i = 0; i < numberOfCarts; i++)
+        {
+            int finalI = i;
+            Platform.runLater(() -> Main.root.getChildren().remove(carts[0][finalI]));
+            Platform.runLater(() -> Main.root.getChildren().remove(carts[1][finalI]));
+            Platform.runLater(() -> Main.root.getChildren().remove(carts[2][finalI]));
+        }
+    }
+
+    private void stopThreads(){
+        P1.stop();
+        P2.stop();
+        P3.stop();
+    }
+    private void initThreads(Shared shared){
+        P1 = new YellowTrain(shared, numberOfCarts,this, route[0]);
+        P2 = new RedTrain(shared, numberOfCarts,this, route[1]);
+        P3 = new BlueTrain(shared, numberOfCarts,this, route[2]);
     }
 
     @FXML
--- src/Projekt/Main.java
+++ src/Projekt/Main.java
@@ -12,14 +12,13 @@ public class Main extends Application
 {
     static AnchorPane root;
     @Override
-    public void start(Stage stage) throws IOException
-    {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("scene1.fxml"));
+    public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("scene.fxml"));
         root = fxmlLoader.load();
         Scene scene = new Scene(root);
-        Image icon = new Image("icon.png");
+        Image icon = new Image("src/resources/icon.png");
         stage.getIcons().add(icon);
-        stage.setTitle("Symulator Metra");
+        stage.setTitle("Subway Simulator");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
